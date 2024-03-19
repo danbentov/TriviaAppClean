696d1317438e2621d2710f44dac0947a5f458e82
User <User@LAB2-10>
2024-03-19 14:41:44 +0200

    game start page

--- ViewModels/GameStartViewModel.cs
+++ ViewModels/GameStartViewModel.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace TriviaAppClean.ViewModels
 {
-    internal class GameStartViewModel
+    public class GameStartViewModel : ViewModelBase
     {
+        public GameStartViewModel()
+        {
+            this.StartGameCommand = new Command(Start);
+        }
+        public ICommand StartGameCommand { get; set; }
+        public async void Start()
+        {
+           await Shell.Current.GoToAsync("triviaGame");
+        }
     }
 }
--- Views/GameStartView.xaml.cs
+++ Views/GameStartView.xaml.cs
@@ -1,9 +1,12 @@
+using TriviaAppClean.ViewModels;
+
 namespace TriviaAppClean.Views;
 
 public partial class GameStartView : ContentPage
 {
-	public GameStartView()
+	public GameStartView(GameStartViewModel vm)
 	{
 		InitializeComponent();
+		this.BindingContext = vm;
 	}
 }
