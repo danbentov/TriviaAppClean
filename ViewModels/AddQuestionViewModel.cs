268997c781bd318eeca2a518611e708544a8d437
User <User@LAB2-9>
2024-03-20 09:34:33 +0200

    finish addQuestion ViewModel

--- ViewModels/AddQuestionViewModel.cs
+++ ViewModels/AddQuestionViewModel.cs
@@ -1,12 +1,176 @@
-﻿using System;
+﻿using AndroidX.CoordinatorLayout.Widget;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TriviaAppClean.Models;
+using TriviaAppClean.Services;
+using TriviaAppClean.Views;
+using static Java.Util.Jar.Attributes;
 
 namespace TriviaAppClean.ViewModels
 {
-    internal class AddQuestionViewModel
+    internal class AddQuestionViewModel:ViewModelBase
     {
+
+        private string errorComment;
+        public string ErrorComment
+        {
+            get
+            {
+                return errorComment;
+            }
+            set
+            {
+                errorComment = value;
+                ValidateComment();
+                OnPropertyChanged();
+            }
+        }
+
+        private bool showErrorComment;
+        public bool ShowErrorComment
+        {
+            get
+            {
+                return showErrorComment;
+            }
+            set
+            {
+                showErrorComment = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void ValidateComment()
+        {
+            if (((App)Application.Current).LoggedInUser.Rank == 2)
+            {
+                this.showErrorComment = true;
+            }
+            else if (((App)Application.Current).LoggedInUser.Score % 100 == 0 && ((App)Application.Current).LoggedInUser.Questions.Count < ((App)Application.Current).LoggedInUser.Score / 100)
+            {
+                this.showErrorComment = true;
+
+            }
+            else
+            {
+                this.showErrorComment = false;
+
+            }
+        }
+
+
+        private string question;
+        public string Question
+        {
+            get
+            {
+                return question;
+            }
+            set
+            {
+                question = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string rightAnswer;
+        public string RightAnswer
+        {
+            get
+            {
+                return rightAnswer;
+            }
+            set
+            {
+                rightAnswer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string wrongAnswer1;
+        public string WrongAnswer1
+        {
+            get
+            {
+                return wrongAnswer1;
+            }
+            set
+            {
+                wrongAnswer1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string wrongAnswer2;
+        public string WrongAnswer2
+        {
+            get
+            {
+                return wrongAnswer2;
+            }
+            set
+            {
+                wrongAnswer2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string wrongAnswer3;
+        public string WrongAnswer3
+        {
+            get
+            {
+                return wrongAnswer3;
+            }
+            set
+            {
+                wrongAnswer3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private TriviaWebAPIProxy triviaService;
+        private ConnectingToServerView connectingToServerView;
+        public AddQuestionViewModel(TriviaWebAPIProxy service, ConnectingToServerView connect)
+        {
+            this.triviaService = service;
+            this.connectingToServerView = connect;
+            this.ErrorComment = "אינך רשאי להוסיף שאלה";
+            this.ShowErrorComment = false;
+            this.AddQuestionCommand = new Command(OnAddQuestion);
+        }
+
+        public ICommand AddQuestionCommand { get; set; }
+
+        private async void OnAddQuestion()
+        {
+            AmericanQuestion quest = new AmericanQuestion();
+            quest.QText = question;
+            quest.CorrectAnswer = rightAnswer;
+            quest.Bad1 = wrongAnswer1;
+            quest.Bad2 = wrongAnswer2;
+            quest.Bad3 = wrongAnswer3;
+            quest.UserId = ((App)Application.Current).LoggedInUser.Id;
+            quest.Status = 0;
+            await Application.Current.MainPage.Navigation.PushModalAsync(connectingToServerView);
+            bool a = await this.triviaService.PostNewQuestion(quest);
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+
+            if (a == true)
+            {
+                await Application.Current.MainPage.DisplayAlert("Add Qustion", "Add Qustion succeed !!", "ok");
+                Application.Current.MainPage = new AppShell();
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Add Qustion", "Add Qustion Failed", "ok");
+            }
+
+        }
+
     }
 }
